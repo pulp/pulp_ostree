35c4c6bdd152a55eff24e1526d9350bb5d44e206
Dennis Kliban <dkliban@redhat.com>
2015-10-07 16:57:54 -0400

    Bumping version to 1.0.0-0.4.beta

--- plugins/setup.py
+++ plugins/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_ostree_plugins',
-    version='1.0.0b3',
+    version='1.0.0b4',
     packages=find_packages(),
     url='http://www.pulpproject.org',
     license='GPLv2+',
