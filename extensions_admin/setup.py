7a22215a5f5f8adc1bda64430d0077c9ea2258f2
Austin Macdonald <asmacdo@gmail.com>
2015-11-05 11:04:32 -0500

    Bumping version to 1.0.0-1

--- extensions_admin/setup.py
+++ extensions_admin/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_ostree_extensions_admin',
-    version='1.0.0c7',
+    version='1.0.0',
     packages=find_packages(),
     url='http://www.pulpproject.org',
     license='GPLv2+',
