92273836d9657dd5450286d1bdfc147f947921a8
Sean Myers <sean.myers@redhat.com>
2016-06-14 09:53:57 -0400

    Bumping version to 1.1.2-0.1.beta

--- extensions_admin/setup.py
+++ extensions_admin/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_ostree_extensions_admin',
-    version='1.1.1',
+    version='1.1.2b1',
     packages=find_packages(exclude=['test']),
     url='http://www.pulpproject.org',
     license='GPLv2+',
--- pulp-ostree.spec
+++ pulp-ostree.spec
@@ -6,8 +6,8 @@
 
 
 Name: pulp-ostree
-Version: 1.1.1
-Release: 1%{?dist}
+Version: 1.1.2
+Release: 0.1.beta%{?dist}
 Summary: Support for OSTree content in the Pulp platform
 Group: Development/Languages
 License: GPLv2
