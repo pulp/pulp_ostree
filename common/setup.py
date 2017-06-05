411f8fc42ca4f42d66593b57b0216cbda30022f7
werwty <bihan.zh@gmail.com>
2017-06-05 11:42:56 -0400

    Update setup.py for pulp3 PyPI packaging
    ref #2444
    https://pulp.plan.io/issues/2444

--- common/setup.py
+++ common/setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pulp_ostree_common',
-    version='1.3a1',
+    name='pulp-ostree-common',
+    version='2.0.0a1.dev0',
     packages=find_packages(exclude=['test']),
     url='http://www.pulpproject.org',
     license='GPLv2+',
     author='Pulp Team',
     author_email='pulp-list@redhat.com',
-    description='common code for pulp\'s ostree support',
+    description='Common code for Pulp\'s OSTree support',
 )
--- plugins/setup.py
+++ plugins/setup.py
@@ -2,16 +2,21 @@
 
 from setuptools import setup, find_packages
 
+requirements = [
+    'pulpcore-plugin',
+    'pulp-ostree-common'
+]
 
 setup(
-    name='pulp_ostree_plugins',
-    version='1.3a1',
-    packages=find_packages(exclude=['test', 'test.*']),
+    name='pulp-ostree',
+    version='2.0.0a1.dev0',
+    packages=find_packages(exclude=['test']),
     url='http://www.pulpproject.org',
+    install_requires=requirements,
     license='GPLv2+',
     author='Pulp Team',
     author_email='pulp-list@redhat.com',
-    description='plugins for ostree support in pulp',
+    description='Plugin to enable OSTree support in Pulp',
     entry_points={
         'pulp.importers': [
             'importer = pulp_ostree.plugins.importers.web:entry_point',
