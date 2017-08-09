09922cd1f95472bb72b40d6c3163fead01a37aac
Patrick Creech <pcreech@redhat.com>
2017-08-09 13:37:07 -0400

    Bumping version to 1.3.0-1

--- plugins/setup.py
+++ plugins/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_ostree_plugins',
-    version='1.3c3',
+    version='1.3',
     packages=find_packages(exclude=['test', 'test.*']),
     url='http://www.pulpproject.org',
     license='GPLv2+',
