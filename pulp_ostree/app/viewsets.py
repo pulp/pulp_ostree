dd10ba8d264faa6a6ab6199c3b67929ad88064a3
Lubos Mjachky <lubos.mjachky@gmail.com>
2021-07-20 10:33:24 +0200

    Fix docs
    [noissue]

--- pulp_ostree/app/viewsets.py
+++ pulp_ostree/app/viewsets.py
@@ -2,7 +2,7 @@
 Check `Plugin Writer's Guide`_ for more details.
 
 .. _Plugin Writer's Guide:
-    http://docs.pulpproject.org/en/3.0/nightly/plugins/plugin-writer/index.html
+    https://docs.pulpproject.org/pulpcore/plugins/plugin-writer/index.html
 """
 
 from django.db import transaction
