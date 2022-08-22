pulp/pulp_cookbook

Update CI files
[noissue]

.github/workflows/scripts/install.sh
@@ -17,11 +17,6 @@ source .github/workflows/scripts/utils.sh
 
 export PULP_API_ROOT="/pulp/"
 
-if [[ "$TEST" = "docs" || "$TEST" = "publish" ]]; then
-  pip install -r ../pulpcore/doc_requirements.txt
-  pip install -r doc_requirements.txt
-fi
-
 cd .ci/ansible/
 
 TAG=ci_build
@@ -122,6 +117,11 @@ fi
 
 ansible-playbook build_container.yaml
 ansible-playbook start_container.yaml
+
+if [[ "$TEST" = "docs" || "$TEST" = "publish" ]]; then
+  cmd_prefix bash -c "cd pulpcore; pip install -r doc_requirements.txt"
+  cmd_prefix bash -c "cd pulp_cookbook; pip install -r doc_requirements.txt"
+fi
 echo ::group::SSL
 # Copy pulp CA
 sudo docker cp pulp:/etc/pulp/certs/pulp_webserver.crt /usr/local/share/ca-certificates/pulp_webserver.crt

.github/workflows/scripts/script.sh
@@ -33,10 +33,8 @@ if [[ "$TEST" = "docs" ]]; then
     pip install towncrier==19.9.0
     towncrier --yes --version 4.0.0.ci
   fi
-  cd docs
-  make PULP_URL="$PULP_URL" diagrams html
-  tar -cvf docs.tar ./_build
-  cd ..
+  cmd_prefix bash -c "cd pulp_cookbook/docs; make diagrams html; tar -cvf docs.tar ./_build"
+  docker cp pulp:/pulp_cookbook//docs/docs.tar docs.tar
 
   if [ -f $POST_DOCS_TEST ]; then
     source $POST_DOCS_TEST

pulp/pulp_ostree

.github/workflows/scripts/install.sh
@@ -17,11 +17,6 @@ source .github/workflows/scripts/utils.sh
 
 export PULP_API_ROOT="/pulp/"
 
-if [[ "$TEST" = "docs" || "$TEST" = "publish" ]]; then
-  pip install -r ../pulpcore/doc_requirements.txt
-  pip install -r doc_requirements.txt
-fi
-
 cd .ci/ansible/
 
 TAG=ci_build
@@ -122,6 +117,11 @@ fi
 
 ansible-playbook build_container.yaml
 ansible-playbook start_container.yaml
+
+if [[ "$TEST" = "docs" || "$TEST" = "publish" ]]; then
+  cmd_prefix bash -c "cd pulpcore; pip install -r doc_requirements.txt"
+  cmd_prefix bash -c "cd pulp_ostree; pip install -r doc_requirements.txt"
+fi
 echo ::group::SSL
 # Copy pulp CA
 sudo docker cp pulp:/etc/pulp/certs/pulp_webserver.crt /usr/local/share/ca-certificates/pulp_webserver.crt

.github/workflows/scripts/script.sh
@@ -33,10 +33,8 @@ if [[ "$TEST" = "docs" ]]; then
     pip install towncrier==19.9.0
     towncrier --yes --version 4.0.0.ci
   fi
-  cd docs
-  make PULP_URL="$PULP_URL" diagrams html
-  tar -cvf docs.tar ./_build
-  cd ..
+  cmd_prefix bash -c "cd pulp_ostree/docs; make diagrams html; tar -cvf docs.tar ./_build"
+  docker cp pulp:/pulp_ostree//docs/docs.tar docs.tar
 
   if [ -f $POST_DOCS_TEST ]; then
     source $POST_DOCS_TEST