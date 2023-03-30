eebf5fd5238bbb73d507c772fc1a030ccbd8c38f
Lubos Mjachky <lmjachky@redhat.com>
2023-03-30 14:08:17 +0200

    Bump to 2.1.0.dev
    [noissue]

--- pulp_ostree/app/__init__.py
+++ pulp_ostree/app/__init__.py
@@ -6,5 +6,5 @@ class PulpOstreePluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_ostree.app"
     label = "ostree"
-    version = "2.0.0.dev"
+    version = "2.1.0.dev"
     python_package_name = "pulp-ostree"
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md") as f:
 
 setup(
     name="pulp-ostree",
-    version="2.0.0.dev",
+    version="2.1.0.dev",
     description="OSTree plugin for the Pulp Project",
     long_description=long_description,
     long_description_content_type="text/markdown",
