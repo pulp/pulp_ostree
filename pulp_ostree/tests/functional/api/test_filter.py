4273c0f6115843b216264df72537b33eb5a2c041
git-hyagi <45576767+git-hyagi@users.noreply.github.com>
2024-01-12 13:51:51 -0300

    Rewrite `test_filter.py` using pytest
    fixes: #324

--- pulp_ostree/tests/functional/api/test_filter.py
+++ pulp_ostree/tests/functional/api/test_filter.py
@@ -1,100 +1,67 @@
-import unittest
+import pytest
 
-from urllib.parse import urlparse
-
-from pulp_smash import config
-from pulp_smash.pulp3.bindings import delete_orphans, monitor_task
-from pulp_smash.pulp3.utils import gen_repo
-
-from pulp_ostree.tests.functional.utils import (
-    gen_ostree_client,
-    gen_ostree_remote,
-)
-from pulp_ostree.tests.functional.utils import set_up_module as setUpModule  # noqa:F401
-
-from pulpcore.client.pulp_ostree import (
-    ContentCommitsApi,
-    ContentRefsApi,
-    RepositoriesOstreeApi,
-    RepositoriesOstreeVersionsApi,
-    RepositorySyncURL,
-    RemotesOstreeApi,
-)
+from pulp_ostree.tests.functional.constants import OSTREE_FIXTURE_URL
 
 
-class ContentFilterTestCase(unittest.TestCase):
-    """A test case that verifies the content filtering."""
+@pytest.fixture
+def synced_repo(
+    ostree_repositories_api_client, ostree_repository_factory, ostree_remote_factory, monitor_task
+):
+    """
+    Fixture that syncs a Remote ostree Repository,
+    waits until the sync task completes and returns the
+    created repository object.
+    """
+    repo = ostree_repository_factory()
+    remote = ostree_remote_factory(url=OSTREE_FIXTURE_URL, policy="immediate")
+    result = ostree_repositories_api_client.sync(repo.pulp_href, {"remote": remote.pulp_href})
+    monitor_task(result.task)
+    return ostree_repositories_api_client.read(repo.pulp_href)
 
-    @classmethod
-    def setUpClass(cls):
-        """Initialize class-wide variables."""
-        cfg = config.get_config()
-        cls.registry_name = urlparse(cfg.get_base_url()).netloc
 
-        client_api = gen_ostree_client()
-        cls.repositories_api = RepositoriesOstreeApi(client_api)
-        cls.versions_api = RepositoriesOstreeVersionsApi(client_api)
-        cls.remotes_api = RemotesOstreeApi(client_api)
+@pytest.mark.parallel
+def test_filter_refs(
+    ostree_content_refs_api_client,
+    ostree_content_commits_api_client,
+    synced_repo,
+):
+    """Check if refs can be filtered by their names and related commits' checksums."""
+    # check by name
+    refs_stable = ostree_content_refs_api_client.list(
+        repository_version_added=synced_repo.latest_version_href, name="stable"
+    ).to_dict()
 
-        cls.commits_api = ContentCommitsApi(client_api)
-        cls.refs_api = ContentRefsApi(client_api)
+    assert refs_stable["count"] == 1, refs_stable
+    assert refs_stable["results"][0]["name"] == "stable", refs_stable
 
-    @classmethod
-    def tearDownClass(cls):
-        """Clean orphaned content after finishing the tests."""
-        delete_orphans()
+    # check by commit
+    commit = ostree_content_commits_api_client.read(refs_stable["results"][0]["commit"])
+    refs_commit_checksum = ostree_content_refs_api_client.list(
+        repository_version_added=synced_repo.latest_version_href, checksum=commit.checksum
+    ).to_dict()
 
-    def setUp(self):
-        """Clean orphaned content before each test."""
-        delete_orphans()
+    assert refs_commit_checksum["count"] == 1, refs_commit_checksum
+    assert refs_commit_checksum["results"][0]["checksum"] == commit.checksum, refs_commit_checksum
 
-        self.repo = self.repositories_api.create(gen_repo())
-        self.addCleanup(self.repositories_api.delete, self.repo.pulp_href)
 
-        body = gen_ostree_remote(depth=0, policy="immediate")
-        remote = self.remotes_api.create(body)
-        self.addCleanup(self.remotes_api.delete, remote.pulp_href)
+@pytest.mark.parallel
+def test_filter_commits(
+    ostree_content_refs_api_client,
+    ostree_content_commits_api_client,
+    synced_repo,
+):
+    """Check if commits can be filtered by their checksums."""
+    refs_rawhide = ostree_content_refs_api_client.list(
+        repository_version_added=synced_repo.latest_version_href, name="rawhide"
+    ).to_dict()
+    ref_rawhide = refs_rawhide["results"][0]
 
-        self.assertEqual(self.repo.latest_version_href, f"{self.repo.pulp_href}versions/0/")
-        repository_sync_data = RepositorySyncURL(remote=remote.pulp_href)
-        response = self.repositories_api.sync(self.repo.pulp_href, repository_sync_data)
-        repo_version = monitor_task(response.task).created_resources[0]
+    commits_rawhide = ostree_content_commits_api_client.list(
+        repository_version_added=synced_repo.latest_version_href,
+        checksum=ref_rawhide["checksum"],
+    ).to_dict()
+    assert commits_rawhide["count"] == 1, commits_rawhide
 
-        self.repository_version = self.versions_api.read(repo_version)
-
-    def test_filter_refs(self):
-        """Check if refs can be filtered by their names and related commits' checksums."""
-        refs_stable = self.refs_api.list(
-            repository_version_added=self.repository_version.pulp_href, name="stable"
-        ).to_dict()
-
-        self.assertEqual(refs_stable["count"], 1, refs_stable)
-        self.assertEqual(refs_stable["results"][0]["name"], "stable", refs_stable)
-
-        commit = self.commits_api.read(refs_stable["results"][0]["commit"])
-
-        refs_commit_checksum = self.refs_api.list(
-            repository_version_added=self.repository_version.pulp_href, checksum=commit.checksum
-        ).to_dict()
-
-        self.assertEqual(refs_commit_checksum["count"], 1, refs_commit_checksum)
-        self.assertEqual(
-            refs_commit_checksum["results"][0]["checksum"], commit.checksum, refs_commit_checksum
-        )
-
-    def test_filter_commits(self):
-        """Check if commits can be filtered by their checksums."""
-        refs_rawhide = self.refs_api.list(
-            repository_version_added=self.repository_version.pulp_href, name="rawhide"
-        ).to_dict()
-        ref_rawhide = refs_rawhide["results"][0]
-
-        commits_rawhide = self.commits_api.list(
-            repository_version_added=self.repository_version.pulp_href,
-            checksum=ref_rawhide["checksum"],
-        ).to_dict()
-        self.assertEqual(commits_rawhide["count"], 1, commits_rawhide)
-
-        commit_rawhide = commits_rawhide["results"][0]
-        self.assertEqual(commit_rawhide["checksum"], ref_rawhide["checksum"], commit_rawhide)
-        self.assertEqual(commit_rawhide["pulp_href"], ref_rawhide["commit"], commit_rawhide)
+    commit_rawhide = commits_rawhide["results"][0]
+    assert commit_rawhide["checksum"] == ref_rawhide["checksum"], commit_rawhide
+    assert commit_rawhide["pulp_href"] == ref_rawhide["commit"], commit_rawhide
