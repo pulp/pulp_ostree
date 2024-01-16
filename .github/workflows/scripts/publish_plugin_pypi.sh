6aec3841144743ee19126263b6f5280cd4f7880e
pulpbot <pulp-infra@redhat.com>
2024-01-16 23:30:51 +0000

    Update CI files
    [noissue]

--- .github/workflows/scripts/publish_plugin_pypi.sh
+++ .github/workflows/scripts/publish_plugin_pypi.sh
@@ -27,7 +27,7 @@ then
   exit
 fi
 
-twine upload -u pulp -p "$PYPI_PASSWORD" \
+twine upload -u __token__ -p "$PYPI_API_TOKEN" \
 "dist/pulp_ostree-$VERSION-py3-none-any.whl" \
 "dist/pulp-ostree-$VERSION.tar.gz" \
 ;
