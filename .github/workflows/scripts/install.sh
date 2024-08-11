6c5a56ea8d430b7d36663c7dde42e445049e2b1e
pulpbot <pulp-infra@redhat.com>
2024-08-11 10:35:25 +0000

    Update CI files
    [noissue]

--- .github/workflows/scripts/install.sh
+++ .github/workflows/scripts/install.sh
@@ -21,18 +21,13 @@ PLUGIN_SOURCE="./pulp_ostree/dist/pulp_ostree-${PLUGIN_VERSION}-py3-none-any.whl
 export PULP_API_ROOT="/pulp/"
 
 PIP_REQUIREMENTS=("pulp-cli-ostree")
-if [[ "$TEST" = "docs" || "$TEST" = "publish" ]]
-then
-  PIP_REQUIREMENTS+=("-r" "doc_requirements.txt")
-fi
 
+# This must be the **only** call to "pip install" on the test runner.
 pip install ${PIP_REQUIREMENTS[*]}
 
-if [[ "$TEST" != "docs" ]]
-then
-  PULP_CLI_VERSION="$(pip freeze | sed -n -e 's/pulp-cli-ostree==//p')"
-  git clone --depth 1 --branch "$PULP_CLI_VERSION" https://github.com/pulp/pulp-cli-ostree.git ../pulp-cli-ostree
-fi
+# Check out the pulp-cli-ostree branch matching the installed version.
+PULP_CLI_VERSION="$(pip freeze | sed -n -e 's/pulp-cli-ostree==//p')"
+git clone --depth 1 --branch "$PULP_CLI_VERSION" https://github.com/pulp/pulp-cli-ostree.git ../pulp-cli-ostree
 
 cd .ci/ansible/
 if [ "$TEST" = "s3" ]; then
@@ -126,9 +121,7 @@ if [ "${PULP_API_ROOT:-}" ]; then
 fi
 
 pulp config create --base-url https://pulp --api-root "$PULP_API_ROOT" --username "admin" --password "password"
-if [[ "$TEST" != "docs" ]]; then
-  cp ~/.config/pulp/cli.toml "${REPO_ROOT}/../pulp-cli-ostree/tests/cli.toml"
-fi
+cp ~/.config/pulp/cli.toml "${REPO_ROOT}/../pulp-cli-ostree/tests/cli.toml"
 
 ansible-playbook build_container.yaml
 ansible-playbook start_container.yaml
