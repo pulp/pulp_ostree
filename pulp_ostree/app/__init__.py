01a90fc69a30b254b2cc4b2c4d602d73e86eff50
pulpbot <pulp-infra@redhat.com>
2024-10-30 15:21:50 +0000

    Bump version: 2.4.4 → 2.4.5.dev

--- pulp_ostree/app/__init__.py
+++ pulp_ostree/app/__init__.py
@@ -6,6 +6,6 @@ class PulpOstreePluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_ostree.app"
     label = "ostree"
-    version = "2.4.4"
+    version = "2.4.5.dev"
     python_package_name = "pulp-ostree"
     domain_compatible = True
