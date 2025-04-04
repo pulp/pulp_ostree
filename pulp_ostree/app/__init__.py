53e8c693dc88b482d364f8b7e4f9db94df81ac87
pulpbot <pulp-infra@redhat.com>
2025-04-04 13:34:29 +0000

    Release 2.4.7

--- pulp_ostree/app/__init__.py
+++ pulp_ostree/app/__init__.py
@@ -6,6 +6,6 @@ class PulpOstreePluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_ostree.app"
     label = "ostree"
-    version = "2.4.7.dev"
+    version = "2.4.7"
     python_package_name = "pulp-ostree"
     domain_compatible = True
