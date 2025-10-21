b0f864d8bde047000181991ef196d89dfcce9f6e
pulpbot <pulp-infra@redhat.com>
2025-10-21 21:35:14 +0000

    Update CI files

--- .github/workflows/scripts/publish_client_pypi.sh
+++ .github/workflows/scripts/publish_client_pypi.sh
@@ -22,5 +22,5 @@ fi
 
 twine upload -u __token__ -p "${PYPI_API_TOKEN}" \
 "dist/pulp_ostree_client-${VERSION}-py3-none-any.whl" \
-"dist/pulp_ostree-client-${VERSION}.tar.gz" \
+"dist/pulp_ostree_client-${VERSION}.tar.gz" \
 ;
