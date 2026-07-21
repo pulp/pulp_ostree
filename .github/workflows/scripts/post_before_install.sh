e83bb6e37c1fd959f48cc8a5d0474a9e486fb57b
Daniel Alley <dalley@redhat.com>
2026-07-21 11:42:32 -0400

    Update to the CentOS Stream 10 CI image

--- .github/workflows/scripts/post_before_install.sh
+++ .github/workflows/scripts/post_before_install.sh
@@ -3,3 +3,8 @@ then
   sudo apt-get update
   sudo apt-get install -y libgirepository1.0-dev libostree-dev
 fi
+
+# PyGObject must match the system GLib version, so exempt it from lowerbounds pinning
+if [[ "$TEST" = "lowerbounds" ]]; then
+  sed -i '/PyGObject/d' lowerbounds_constraints.txt
+fi
