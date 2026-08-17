d33aadcc7ded36189c5e919ceaaffad3bd9556ad
Jitka Halova <jobselko@redhat.com>
2026-08-17 13:36:09 +0200

    Tmp: use cli-ostree fork for fix verification

--- .github/workflows/scripts/install.sh
+++ .github/workflows/scripts/install.sh
@@ -32,9 +32,8 @@ then
 fi
 fi
 
-# Check out the pulp-cli-ostree branch matching the installed version.
-PULP_CLI_VERSION="$(pip freeze | sed -n -e 's/pulp-cli-ostree==//p')"
-git clone --depth 1 --branch "$PULP_CLI_VERSION" https://github.com/pulp/pulp-cli-ostree.git ../pulp-cli-ostree
+# Temporarily use PR branch for debugging
+git clone --depth 1 --branch "fix_tests" https://github.com/jobselko/pulp-cli-ostree.git ../pulp-cli-ostree
 
 PULP_API_ROOT="$(yq -r '.pulp_scenario_settings.api_root // .pulp_settings.api_root // "/pulp/"' < .ci/ansible/vars/main.yaml)"
 
