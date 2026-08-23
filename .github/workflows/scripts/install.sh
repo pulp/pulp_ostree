3a0c97247090fe5cc68cc071280924c7912aa145
pulpbot <pulp-infra@redhat.com>
2026-08-23 10:41:12 +0000

    Update CI files

--- .github/workflows/scripts/install.sh
+++ .github/workflows/scripts/install.sh
@@ -20,18 +20,6 @@ PIP_REQUIREMENTS=("pulp-cli-ostree" "yq")
 # This must be the **only** package install on the test runner.
 uv pip install ${PIP_REQUIREMENTS[*]}
 
-if [[ "$TEST" = "s3" ]]; then
-for i in {1..3}
-do
-  ansible-galaxy collection install "amazon.aws:11.1.0" && s=0 && break || s=$? && sleep 3
-done
-if [[ $s -gt 0 ]]
-then
-  echo "Failed to install amazon.aws"
-  exit $s
-fi
-fi
-
 # Check out the pulp-cli-ostree branch matching the installed version.
 PULP_CLI_VERSION="$(uv pip freeze | sed -n -e 's/pulp-cli-ostree==//p')"
 git clone --depth 1 --branch "$PULP_CLI_VERSION" https://github.com/pulp/pulp-cli-ostree.git ../pulp-cli-ostree
